7ba8b91f5d4a32d30f6ea975c2ec3964da769904
Aryan-Maurya <harshuvardhan14@gmail.com>
2026-07-20 00:50:03 +0530

    chore: make assets base path conditional for vercel compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/school-demo-web/',
+  base: process.env.GITHUB_ACTIONS ? '/school-demo-web/' : '/',
   plugins: [
     react(),
     tailwindcss(),
